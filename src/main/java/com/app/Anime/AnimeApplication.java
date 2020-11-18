a090208d94db6f41391ec37448867894d20bb27a
vaishuvk <71807351+vaishuvk@users.noreply.github.com>
2020-11-18 10:50:16 +0530

    Update AnimeApplication.java

--- src/main/java/com/app/Anime/AnimeApplication.java
+++ src/main/java/com/app/Anime/AnimeApplication.java
@@ -8,6 +8,11 @@ public class AnimeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimeApplication.class, args);
+		
+		String port=System.getenv("PORT");
+		String host=System.getenv("HOST");
+		String my_sqluser=System.getenv("MY_SQLUSER");
+		String my_sqlpass=System.getenv("MY_SQLPASS");
 	}
 
 }
